9e31ad9e7c4f89819483b697d7fd54b3589b04e4
Brent Marquez <brentonmarquez@gmail.com>
2024-04-13 18:02:24 -0600

    add json tags to model struct to convert to idiomatic field names in response

--- internal/data/books.go
+++ internal/data/books.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Book struct {
-	ID int64
-	CreatedAt time.Time
-	Title string
-	Published int 
-	Pages int 
-	Genres []string 
-	Rating float32 
-	Version int32
+	ID int64 `json:"id"` // JSON tags allow you to keep idiomatic struct naming conventions, but use JSON conventions when marshalled
+	CreatedAt time.Time `json:"-"` // we don't need this in response, so strip it - use directive
+	Title string `json:"title"`
+	Published int `json:"published",omitempty` // make the field optional by using omit empty
+	Pages int `json:"pages",omitempty`
+	Genres []string `json:"genres",omitempty`
+	Rating float32 `json:"rating",omitempty` // Note: the default val for the data type will be provided: i.e. 0 for float32
+	Version int32 `json:"-"` // system data, discard in marshaling for response
 }
